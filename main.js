30fa36441c618cc90023efc7a663dd5a3288b643
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-08-24 13:08:48 +0200

    Add function to console.log all trains with their delays

--- main.js
+++ main.js
@@ -14,3 +14,34 @@ $('#b1').click(function(){
 		}
 	});
 });
+
+function allTrains(){
+	for (let i = 1; i <= 40000; i++) {
+		$.ajax({
+			type: "GET",
+			url: "http://apiv2.oroszi.net/elvira/maps",
+			dataType: "json",
+			data:{
+				train_number: "55"+i
+			},
+		success: function(data){
+			console.log(data[i].train_number+"\n"+data[i].delay);
+		}
+		})
+	}
+	
+	// $.ajax({
+	// 	content_type:"GET",
+	// 	url: "http://apiv2.oroszi.net/elvira/maps",
+	// 	dataType: "JSON",
+	// 	data:{
+	// 		start_station: "Békéscsaba",
+	// 	},
+	// 	success: function(data){
+	// 		console.log(data.end_station);
+			
+	// 	}
+
+	// });
+
+}

63e2a286cb03fbec01f7688c8468e69a137eb589
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-08-24 15:09:11 +0200

    Fix uncaught TypeError

--- main.js
+++ main.js
@@ -16,18 +16,43 @@ $('#b1').click(function(){
 
 });
 
-$( document ).ready(function() {
-    for (let i = 1; i <= 40000; i++) {
-        $.ajax({
-            type: "GET",
-            url: "http://apiv2.oroszi.net/elvira/maps",
-            dataType: "json",
-            data:{
-                train_number: "55"+i
-            },
-        success: function(data){
-            console.log(data[i].train_number+"\n"+data[i].delay);
-        }
-        })
-    }
-});
+function delays() {
+		for (let i = 1; i <= 40000; i++) {
+	        $.ajax({
+	            type: "GET",
+	            url: "http://apiv2.oroszi.net/elvira/maps",
+	            dataType: "json",
+	            data:{
+	            },
+	        success: function(data){
+				if(data[i]!==undefined){ 
+					console.log(data[i].train_number+"\n"+data[i].delay);
+				}
+				}
+	        })
+	    }
+	}
+
+
+// try{
+// $( document ).ready(function() {
+//     for (let i = 1; i <= 40000; i++) {
+//         $.ajax({
+//             type: "GET",
+//             url: "http://apiv2.oroszi.net/elvira/maps",
+//             dataType: "json",
+//             data:{
+//                 train_number: "55"+i
+//             },
+//         success: function(data){
+//             console.log(data[i].train_number+"\n"+data[i].delay);
+//         }
+//         })
+//     }
+// })
+// }
+// catch{
+
+// }
+
+

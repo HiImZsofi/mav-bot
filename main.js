1777ddd56a1c6089fc42183d475c9d2162b7941b
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-08-26 13:34:06 +0200

    Remove unused and/or broken code

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
+//Imports
 import { createRequire } from 'module'
 const require = createRequire(import.meta.url);
 import fetch from "node-fetch";
 const { get, type } = require('jquery');
 var mysql = require('mysql');
-
 
 var con = mysql.createConnection({
 	host: "127.0.0.1",
@@ -12,50 +12,27 @@ var con = mysql.createConnection({
 	port: 3306
 });
 
+//Stores data fetched from the API
 let trains;
 
+//Fetches data from api and stores it in a variable
 async function fetchData(){
-	
 	const res = await fetch('http://apiv2.oroszi.net/elvira/maps')
-
 	trains = await res.json();
-
-	console.log(trains[1].train_number);
 }
 
-function tableEmtpyCheck(){
-	var empty=true;
-	con.connect(function(err) {  
-		if (err) throw err;  
-		var sql = "SELECT COUNT(*) FROM mavdelays.delays";  
-		con.query(sql, function (err, result) {  
-		if (err) throw err;  
-		empty=false; 
-		})
-	})
-	return empty;
-}
-
+//Inserts into the database 
 function sendToDatabase(){
-	if(tableEmtpyCheck()){ 
-		con.connect(function(err) {  
-			if (err) throw err;  
-			console.log("Connected!"); 
-	
-			for (let i = 0; i < trains.length; i++) {
-			var sql = "INSERT INTO mavdelays.delays (trainID, delay) VALUES ('"+trains[i].train_number+"',"+trains[i].delay+")";  
-			con.query(sql, function (err, result) {  
-			if (err) throw err;  
-			console.log("1 record inserted");  
-			}); 
-			}
-			}	
-		)
-	}else{
-		//update
-	}	
+	for (let i = 0; i < trains.length; i++) {
+	var sql = "INSERT INTO mavdelays.delays (trainID, delay) VALUES ('"+trains[i].train_number+"',"+trains[i].delay+")";  
+	con.query(sql, function (err, result) {  
+	if (err) throw err;  
+	console.log("1 record inserted");  
+	}); 
+	}
 }
 
+//Call the required functions
 fetchData();
 setInterval(() => {
 	sendToDatabase();

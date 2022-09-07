dfe4156e02022abe1530bdd98d6d756bcc98399c
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-09-07 16:27:08 +0200

    Rename variables for better readability

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ async function fetchData(){
 }
 
 //Checks if the data already exists in the database
-var sqlExists;
+let sqlExists;
 function existsQuery(){
 	return new Promise((resolve, rejects)=>{
 		con.query(sqlExists, function(err, result) {
@@ -39,10 +39,10 @@ function existsQuery(){
 }
 
 //Inserts new row into the database
-var sql;
+let insertSQL;
 function insertQuery(){
 	return new Promise((resolve, rejects)=>{
-		con.query(sql, function (err) {  
+		con.query(insertSQL, function (err) {  
 			if (err) return rejects(err);  
 			return resolve("1 record inserted");  
 		});
@@ -50,7 +50,7 @@ function insertQuery(){
 }
 
 //Updates the already existing rows in the database
-var updateSQL;
+let updateSQL;
 function updateQuery(){
 	return new Promise((resolve, rejects)=>{
 		con.query(updateSQL, function(err){
@@ -69,7 +69,7 @@ async function sendToDatabase(){
 		if(trains[i].delay !== undefined){
 			sqlExists = "SELECT EXISTS(SELECT * FROM mavdelays.delays WHERE trainID = '"+trains[i].train_number+"') AS answer;"
 			//sqlDelayDifference = "SELECT delay FROM mavdelays.delays WHERE trainID='"+trains[i].train_number+"'";
-			sql = "INSERT INTO mavdelays.delays (trainID, delay, time) VALUES ('"+trains[i].train_number+"',"+trains[i].delay+", CURRENT_TIMESTAMP)";
+			insertSQL = "INSERT INTO mavdelays.delays (trainID, delay, time) VALUES ('"+trains[i].train_number+"',"+trains[i].delay+", CURRENT_TIMESTAMP)";
 			updateSQL="UPDATE mavdelays.delays SET delay = "+trains[i].delay+", time = CURRENT_TIMESTAMP WHERE trainID = '"+trains[i].train_number+"';";
 
 			//Checks if it should update or insert into the database

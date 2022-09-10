2194416a66d11c887e7e90ce8ddd93b5b530cee1
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-09-10 21:46:45 +0200

    Re-enable only update if changed function

--- main.js
+++ main.js
@@ -27,6 +27,7 @@ let trains;
 async function fetchData(){
 	const res = await fetch('http://apiv2.oroszi.net/elvira/maps')
 	trains = await res.json();
+	console.log("Fetched")
 }
 
 //Checks if the data already exists in the database
@@ -41,15 +42,15 @@ function existsQuery(){
 }
 
 //! not working
-// let sqlDelayDifference;
-// function shouldUpdateQuery(){
-// 	return new Promise((resolve, rejects)=>{
-// 		con.query(sqlDelayDifference, function(err, result) {
-// 			if(err) return rejects(err);
-// 			return resolve(result[0].delay);
-// 		})
-// 	})
-// }
+let sqlDelayDifference;
+function shouldUpdateQuery(){
+	return new Promise((resolve, rejects)=>{
+		con.query(sqlDelayDifference, function(err, result) {
+			if(err) return rejects(err);
+			return resolve(result[0].delay);
+		})
+	})
+}
 
 //Inserts new row into the database
 let insertSQL;
@@ -76,13 +77,13 @@ function updateQuery(){
 //Interacts with the database
 async function sendToDatabase(){
 	var checkExists;
-	// var shouldUpdate;
+	var shouldUpdate;
 
 	for (let i = 0; i < trains.length; i++) {
 		//Checks if the delay value from the API is undefined
 		if(trains[i].delay !== undefined){
 			sqlExists = "SELECT EXISTS(SELECT * FROM mavdelays.delays WHERE trainID = '"+trains[i].train_number+"') AS answer;"
-			// sqlDelayDifference = "SELECT delay FROM mavdelays.delays WHERE trainID = '"+trains[i].train_number+"'";
+			sqlDelayDifference = "SELECT delay FROM mavdelays.delays WHERE trainID = '"+trains[i].train_number+"'";
 			insertSQL = "INSERT INTO mavdelays.delays (trainID, delay, time) VALUES ('"+trains[i].train_number+"',"+trains[i].delay+", CURRENT_TIMESTAMP)";
 			updateSQL="UPDATE mavdelays.delays SET delay = "+trains[i].delay+", time = CURRENT_TIMESTAMP WHERE trainID = '"+trains[i].train_number+"';";
 
@@ -91,10 +92,10 @@ async function sendToDatabase(){
 			if(checkExists == 0){
 				console.log(await insertQuery());
 			}else if(checkExists === 1){ //! second part of if condition not working
-				// shouldUpdate = await shouldUpdateQuery(); //! not working
-				// if (shouldUpdate - trains[i].delay != 0) {
+				shouldUpdate = await shouldUpdateQuery(); //! not working
+				if (shouldUpdate - trains[i].delay != 0) {
 					console.log(await updateQuery());
-				// }
+				}
 			}
 			}
 			else{

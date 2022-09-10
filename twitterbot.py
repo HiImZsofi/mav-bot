ed418ca03ec0659fc2cb09e2d9a3ac4ff3133397
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-09-10 11:04:03 +0200

    Remove unused code

--- twitterbot.py
+++ twitterbot.py
@@ -2,6 +2,7 @@ import tweepy
 #from sum import result
 import schedule
 import time 
+import mysql.connector
 
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler("tiOxv4JZtUPA3GqPwgleFnUgH", "maPuNX6HZscOhodrwc8lpp5tiMVHGbuGzs5HP6JQ13JOVhqMby")
@@ -10,8 +11,6 @@ auth.set_access_token("1567913293381091331-lJTkqqYBkVR79eGMzi3FB8bNkGBjFS", "831
 # Create API object
 api = tweepy.API(auth)
 
-#summary
-import mysql.connector
 
 mydb = mysql.connector.connect(
     host = "127.0.0.1",
@@ -20,21 +19,16 @@ mydb = mysql.connector.connect(
 	port = 3306
 )
 
-#print(mydb)
 obj = mydb.cursor();
 obj.execute("SELECT SUM(delay) FROM mavdelays.delays;");
 result= obj.fetchall()
-#print(result)
 
 def tweet():
     api.update_status(result)
     #print("szia")
-
-
 
 schedule.every().day.at("20:16").do(tweet)
 
 while True:
     schedule.run_pending()
     time.sleep(1)
-    

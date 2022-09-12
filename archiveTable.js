12b8a07309796fc1271265c02991c6aa413a2593
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-09-12 12:36:49 +0200

    Automate archiveTable.js

--- archiveTable.js
+++ archiveTable.js
@@ -2,6 +2,7 @@ import { createRequire } from 'module'
 const require = createRequire(import.meta.url);
 const { get, type } = require('jquery');
 var mysql = require('mysql');
+var cron = require('node-cron');
 
 var con = mysql.createConnection({
 	host: "127.0.0.1",
@@ -10,16 +11,23 @@ var con = mysql.createConnection({
 	port: 3306
 });
 
-let today = new Date();
-let date = today.toISOString().split('T')[0];
+cron.schedule('35 12 * * *', () => {
+    var today = new Date();
+    var date = today.toISOString().split('T')[0];
+    
+    con.query("USE mavdelays;", function(err){
+        if(err) throw err;
+    })
+    
+    con.query("RENAME TABLE delays TO `" + date + "`;", function(err){
+        if(err) throw err;
+        console.log("Table archived as " + date);
+    })
 
-con.query("USE mavdelays;", function(err){
-    if(err) throw err;
-})
-
-con.query("RENAME TABLE delays TO `" + date + "`;", function(err){
-    if(err) throw err;
-    console.log("Table archived as " + date);
-    con.end();
-    process.exit();
-})
+    con.query("CREATE TABLE mavdelays.delays (trainID VARCHAR(255), delay INT, time TIMESTAMP)", function(err){
+        if(err) throw err;
+        console.log("Table created");
+        con.end();
+        process.exit();
+    });
+});

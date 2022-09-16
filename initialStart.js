9a1f1d095a067c7bb7c69e27c7ac745ba16ca3cd
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-09-16 16:38:00 +0200

    Add initialStart.js

---
+++ initialStart.js
@@ -0,0 +1,21 @@
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url);
+const { get, type } = require('jquery');
+import { con } from './database.js'
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+con.query("CREATE DATABASE MavDelays", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+});
+
+con.query("CREATE TABLE mavdelays.delays (trainID VARCHAR(255), delay INT, time TIMESTAMP)", function(err, result){
+	if(err) throw err;
+	console.log("Table created");
+	con.end();
+	process.exit();
+});

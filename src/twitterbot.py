4d2ac973050fa182b2cfbbddab2000bc5b30377c
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-09-19 15:58:03 +0200

    Fix database connection

--- src/twitterbot.py
+++ src/twitterbot.py
@@ -1,7 +1,11 @@
 # Imports
+import os
 import tweepy
 import mysql.connector
 import config
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler(config.consumer_token, config.consumer_secret)
@@ -13,12 +17,14 @@ api = tweepy.API(auth)
 # Connect to database
 mydb = mysql.connector.connect(
     host="127.0.0.1",
-    user="root",
-    password="",
-    port=3306
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    port=os.getenv("DB_PORT")
 )
 
 # Get summed up delay value from the database
+
+
 def sumQuery():
     obj = mydb.cursor()
     obj.execute("SELECT SUM(delay) FROM MavDelays.delays;")
@@ -26,19 +32,13 @@ def sumQuery():
     tweetMessage = "A MÁV ma " + str(result[0][0]) + " percet késett összesen."
     return tweetMessage
 
-
-# def clearTable():
-#     obj = mydb.cursor()
-#     obj.execute("DROP TABLE mavdelays.delays;")
-#     obj.execute(
-#         "CREATE TABLE mavdelays.delays (trainID VARCHAR(255), delay INT, time TIMESTAMP);")
-#     obj.fetchall()
-
-
 # Tweet out results function
+
+
 def tweet():
-    api.update_status(sumQuery())
-    #clearTable()
+    print(sumQuery())
+    # api.update_status(sumQuery())
+    # clearTable()
 
 
 tweet()

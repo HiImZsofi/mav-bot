4fb7e8e5c8d01ff599399e7307911c4864b6ac20
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-09-19 17:35:38 +0200

    Fix crashing on docker build

--- src/initialStart.js
+++ src/initialStart.js
@@ -4,13 +4,13 @@ const { get, type } = require('jquery');
 import { con } from './database.js'
 
 con.connect(function(err) {
-    if (err) throw err;
+    //if (err) throw err;
     console.log("Connected!");
 });
 
 con.query("CREATE TABLE delays (trainID VARCHAR(255), delay INT, time TIMESTAMP)", function(err, result){
-	if(err) throw err;
-	console.log("Table created");
-	con.end();
-	process.exit();
+    //if(err) throw err;
+    console.log("Table created");
+    con.end();
+    process.exit();
 });

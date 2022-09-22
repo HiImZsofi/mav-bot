84ce5cf6026645f6730a32e3a2a433aba4436044
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-09-22 11:49:39 +0200

    Change twitterbot.py to empty the table after tweeting

--- src/twitterbot.py
+++ src/twitterbot.py
@@ -5,7 +5,7 @@ import mysql.connector
 import config
 from dotenv import load_dotenv
 
-#Load envionmental variables
+# Load envionmental variables
 load_dotenv()
 
 # Authenticate to Twitter
@@ -23,21 +23,29 @@ mydb = mysql.connector.connect(
     port=os.getenv("DB_PORT")
 )
 
+obj = mydb.cursor()
 # Get summed up delay value from the database
 
 
 def sumQuery():
-    obj = mydb.cursor()
     obj.execute("SELECT SUM(delay) FROM MavDelays.delays;")
     result = obj.fetchall()
     tweetMessage = "A MÁV ma " + str(result[0][0]) + " percet késett összesen."
     return tweetMessage
 
+
+def emptyTable():
+    obj.execute("TRUNCATE TABLE MavDelays.delays;")
+    obj.fetchall()
+
+
 # Tweet out results function
 
 
 def tweet():
-    #api.update_status(sumQuery())
+    # api.update_status(sumQuery())
     print(sumQuery())
+    emptyTable()
+
 
 tweet()
